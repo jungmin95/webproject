ac902e3ac499687a0e241db8c61396064790bdad
parkjungmin <rhemrgks123@gmail.com>
2019-05-30 20:40:11 +0900

    헤더푸터메인컨트롤러

---
+++ wp/src/main/java/kr/co/wp/controller/MainController.java
@@ -0,0 +1,28 @@
+package kr.co.wp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	
+	
+	//메인페이지
+	@RequestMapping(value="index.do")
+	public String main_mainpage() {
+
+		return "main/main-page";
+	}
+	//게시판 create
+	@RequestMapping(value="/board/create")
+	public String board_create() {
+		return "Board/BoardCreate";
+	}
+	//갤러리 create
+	@RequestMapping(value="/gallery/create")
+	public String gallery_create() {
+		return "Gallery/create";
+	}
+	
+}
