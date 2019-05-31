350d9a7586c608df101622984b3af666b2dfe5bb
parkjungmin <rhemrgks123@gmail.com>
2019-05-31 10:54:43 +0900

    투어리스트 도메인

--- wp/src/main/java/kr/co/wp/domain/Tourlist.java
+++ wp/src/main/java/kr/co/wp/domain/Tourlist.java
@@ -2,12 +2,46 @@ package kr.co.wp.domain;
 
 import java.io.Serializable;
 
-import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.multipart.MultipartFile;
 
-public class Tourlist implements Serializable{
-	private int tourlistId; // 여행지 번호
-	private String tourlistName; // 여행지 이름
-	private String tourlistDesc; // 여행지 소개
-	private String tourlistUrl; // 여행지이미지 경로
-	private MultipartFile tourlistPhoto; // 여행지 이미지 파일
+public class Tourlist /*implements Serializable*/{
+	private int tournum; // 여행지 번호
+	private String userid; // 유저아이디
+	private String tourimg; // 여행지 이미지
+	private String tourname; // 여행지이름
+	private String tourstory; // 여행지소개글
+	private String tourtype; // 여행지 타입
+
+
+	public int getTournum() {
+		return tournum;
+	}
+	public void setTournum(int tournum) {
+		this.tournum = tournum;
+	}
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getTourimg() {
+		return tourimg;
+	}
+	public void setTourimg(String tourimg) {
+		this.tourimg = tourimg;
+	}
+	public String getTourstory() {
+		return tourstory;
+	}
+	public void setTourstory(String tourstory) {
+		this.tourstory = tourstory;
+	}
+	public String getTourtype() {
+		return tourtype;
+	}
+	public void setTourtype(String tourtype) {
+		this.tourtype = tourtype;
+	}
 }
