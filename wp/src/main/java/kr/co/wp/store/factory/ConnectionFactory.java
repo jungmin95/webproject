3a232410f8ce62029734416dbde97f86d0ab3d85
이대희 <eogml4203@gmail.com>
2019-06-05 18:39:50 +0900

    connectionFactory 수정

--- wp/src/main/java/kr/co/wp/store/factory/ConnectionFactory.java
+++ wp/src/main/java/kr/co/wp/store/factory/ConnectionFactory.java
@@ -6,30 +6,30 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-	private static ConnectionFactory instance;
-	
-	private static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	private static final String USER_NAME = "hr";
-	private static final String PASSWORD = "hr";
-	
-	private ConnectionFactory() {
-		try {
-			Class.forName(DRIVER_NAME);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public static ConnectionFactory getInstance() {
-		if(instance == null) {
-			instance = new ConnectionFactory();
-		}
-		return instance;
-		
-	}
-	public Connection createConnection() throws SQLException{
-		return (Connection) DriverManager.getConnection(URL,USER_NAME, PASSWORD);
-	}
+   private static ConnectionFactory instance;
+   
+   private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+   private static final String URL = "jdbc:mysql://61.34.63.180:3306/webproject";
+   private static final String USER_NAME = "root";
+   private static final String PASSWORD = "bjworld21!";
+   
+   private ConnectionFactory() {
+      try {
+         Class.forName(DRIVER_NAME);
+      } catch (ClassNotFoundException e) {
+         e.printStackTrace();
+      }
+      
+   }
+   
+   public static ConnectionFactory getInstance() {
+      if(instance == null) {
+         instance = new ConnectionFactory();
+      }
+      return instance;
+      
+   }
+   public Connection createConnection() throws SQLException{
+      return (Connection) DriverManager.getConnection(URL,USER_NAME, PASSWORD);
+   }
 }
